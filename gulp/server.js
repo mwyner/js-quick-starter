bbdb1380c51ddddea9ffbbe8d4d9da5532700ffa
Matthew Wyner <mwyner@gmail.com>
2016-02-14 23:11:05 -0500

    Added form elements, async submission & POST api endpoint * added form elements to index.html * added get/post endpoints for user management * added async form submit script to main.js * added users.json flat file "database"

--- gulp/server.js
+++ gulp/server.js
@@ -20,8 +20,27 @@ server.use( reloadMW() );
 server.use( '/', express.static(ROOTDIR) );
 
 // supporting JSON flat file "DataBase"
+var USERS_FILE = './src/data/users.json';
 server.use( bodyParser.json() );
 server.use( bodyParser.urlencoded({ extended: true }) );
+
+server.get('/api/get-users', function(req, res) {
+ fs.readFile(USERS_FILE, function(err, data) {
+   res.setHeader('Cache-Control', 'no-cache');
+   res.json(JSON.parse(data));
+ });
+});
+
+server.post('/api/create-user', function(req, res) {
+ fs.readFile(USERS_FILE, function(err, data) {
+   var comments = JSON.parse(data);
+   comments.push(req.body);
+   fs.writeFile(USERS_FILE, JSON.stringify(comments, null, 4), function(err) {
+     res.setHeader('Cache-Control', 'no-cache');
+     res.json(comments);
+   });
+ });
+});
 
 // live reload
 function notify(event) {
@@ -37,3 +56,4 @@ gulp.task('serve', function() {
   liveReload.listen(35729);
   gulp.watch('./dist/**/*', notify);
 });
+
